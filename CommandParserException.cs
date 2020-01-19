1a4847f5cfc9112414de31bab129ac3012cb21d1
Tamas Bolner <bolner@outlook.com>
2020-01-19 20:09:56 +0100

    Fix visibility

--- CommandParserException.cs
+++ CommandParserException.cs
@@ -1,9 +1,23 @@
 using System;
 
 namespace ConController {
-    class CommandParserException : Exception {
+    /// <summary>
+    /// User-friendly error message about errors in the CLI command
+    /// </summary>
+    public class CommandParserException : Exception {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public CommandParserException() : base() { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public CommandParserException(string message) : base(message) { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public CommandParserException(string message, Exception innerException)
             : base(message, innerException) { }
     }
