75a4765246e5c01c28de0db4c6b615affd19d26e
Tamas Bolner <bolner@outlook.com>
2020-03-12 13:28:17 +0100

    By default params are required

--- Annotation.cs
+++ Annotation.cs
@@ -67,6 +67,6 @@ namespace ConController {
         /// If false, then the parameter cannot be omitted. If true then you
         /// can specify a default in your code, like ", int repeat = 3) {"
         /// </summary>
-        public bool Optional { get; set; } = true;
+        public bool Optional { get; set; } = false;
     }
 }
